8718b6282b5d89c8247ceee4243daed4c346590f
STerliakov <terlya.stas@gmail.com>
2025-07-01 18:34:30 +0200

    Add explicit imports to reference `__all__` on

--- substrateinterface/__init__.py
+++ substrateinterface/__init__.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from . import base, contracts, keypair, interfaces, extensions
 from .base import *
 from .contracts import *
 from .keypair import *
